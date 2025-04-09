40788c6d2b50038c9f6fed907c88adf1212aa502
v-enusdelta <157256528+v-enusdelta@users.noreply.github.com>
2025-04-09 13:57:18 -0500

    Modified event create test

--- assignment5/api/tests.py
+++ assignment5/api/tests.py
@@ -10,11 +10,16 @@ class EventTests(TestCase):
     def setUp(self):
         self.client = APIClient()
         self.user = User.objects.create(
-            username='testuser', 
-            email='test@test.com', 
-            password='testpassword')
+            userid=1,
+            username= 'testuser',
+            password= 'testpassword',
+            email='test@test.com',
+            isadmin=False,
+            isreporter=False,
+            isanalyst=False,
+            isviewer=True)
         self.session = Session.objects.create(
-            user=self.user, 
+            userid=self.user.userid, 
             sessionid='session123', 
             sessionkey='session_key_123',
             created_at=timezone.now(),
@@ -23,8 +28,10 @@ class EventTests(TestCase):
 
     def test_event_viewset_create(self):
         payload = {
+            'eventid': 1,
             'sessionid': self.session.sessionid,
             'itemid': 'item123',
+            'eventtime': timezone.now(),
             'eventtype': 'modify'
         }
 
