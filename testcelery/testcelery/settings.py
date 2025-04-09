afcf1c4c52a29368eef3f3c0f13b9e946c524d01
v-enusdelta <157256528+v-enusdelta@users.noreply.github.com>
2025-04-09 09:10:51 -0500

    Added materials for learning celery

--- testcelery/testcelery/settings.py
+++ testcelery/testcelery/settings.py
@@ -121,3 +121,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Email placeholder setting
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
